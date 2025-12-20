9a46959528cf2325ded01f1c1ca5ca8bfe54acea
Joe Sak <joe@joesak.com>
2025-12-19 22:47:20 -0700

    Dbug

--- app/main.ts
+++ app/main.ts
@@ -56,6 +56,7 @@ const getPath = async command => {
 
 const callback = async input => {
   const exePath = await getPath(input)
+  console.log(typeof exepath)
 
   switch (true) {
     case input === "exit":
@@ -67,7 +68,7 @@ const callback = async input => {
     case ECHO_PATTERN.test(input):
       echo(input)
       break
-    case exePath !== undefined:
+    case typeof exePath !== undefined:
       const result = execSync(input)
       rl.output.write(result)
       break
