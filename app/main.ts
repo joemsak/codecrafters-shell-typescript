b0e364d3686316dc0625f472554041ca563f3ac4
Joe Sak <joe@joesak.com>
2025-12-19 22:47:19 -0700

    Add the type builtin

--- app/main.ts
+++ app/main.ts
@@ -5,11 +5,24 @@ const rl = createInterface({
   output: process.stdout,
 })
 
+const BUILTINS = ['echo', 'exit', 'type'];
+
 const ECHO_PATTERN = /^echo\s/
+const TYPE_PATTERN = /^type\s/
 
 const echo = input => {
   const toEcho = input.replace(ECHO_PATTERN, "")
   console.log(toEcho)
+}
+
+const type = input => {
+  const command = input.replace(TYPE_PATTERN, "")
+
+  if (BUILTINS.include(command)) {
+    console.log(`${command} is a shell builtin`)
+  } else {
+    console.error(`${command}: not found`)
+  }
 }
 
 const callback = input => {
@@ -17,6 +30,9 @@ const callback = input => {
     case input === "exit":
       rl.close()
       return
+    case TYPE_PATTERN.test(input):
+      type(input)
+      break
     case ECHO_PATTERN.test(input):
       echo(input)
       break
