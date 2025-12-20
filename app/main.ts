60828dbc950d876a63549eaa0933f679b4a62200
Joe Sak <joe@joesak.com>
2025-12-19 22:47:19 -0700

    Add echo builtin

--- app/main.ts
+++ app/main.ts
@@ -11,6 +11,8 @@ const callback = input => {
     return;
   }
 
+  if (/\Aecho/.match?(input)) console.log(`${input}\n`);
+
   console.error(`${input}: command not found`);
   rl.question("$ ", callback);
 };
